80c7a349dd25029cdec0bcc9da74aa9931abe346
Mr Devil <168351611+mrdevil42023@users.noreply.github.com>
2024-07-30 08:26:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
+OWNER_ID = int(getenv("OWNER_ID", 6107980637))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/devilbotsupport")
