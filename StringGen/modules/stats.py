9a003664333c1afd1319010c8806ccb2a0559356
Mr Devil <168351611+mrdevil42023@users.noreply.github.com>
2024-08-21 23:47:57 +0530

    Update stats.py

--- StringGen/modules/stats.py
+++ StringGen/modules/stats.py
@@ -6,7 +6,7 @@ from StringGen import Anony
 from StringGen.utils import get_served_users
 
 
-@Anony.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
+@Anony.on_message(filters.command(["status", "users"]) & filters.user(OWNER_ID))
 async def get_stats(_, message: Message):
     users = len(await get_served_users())
     await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {Anony.name} :\n\n {users} ᴜsᴇʀs")
